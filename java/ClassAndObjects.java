a43174efbc12071cd05be19fd55d38f80a87ceca
Kmuzaki <keihanmuzaki0480@gmail.com>
2025-09-03 16:17:35 +0800

    initJavaClass

---
+++ java/ClassAndObjects.java
@@ -0,0 +1,18 @@
+package java;
+
+public class ClassAndObjects {
+    String name;
+
+    public ClassAndObjects(String name) {
+        this.name = name;
+    }
+    public static void main(String[] args) {
+        ClassAndObjects class1 = new ClassAndObjects("Visual Programming");
+        ClassAndObjects class2 = new ClassAndObjects("Web Development");
+        System.out.println("Class name: " + class1.name);
+        System.out.println("Class name: " + class2.name);
+    }
+}
+
+
+    

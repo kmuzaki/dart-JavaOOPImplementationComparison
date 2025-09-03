8c9e30028902095734cab0d147324e60d76d4ea0
Kmuzaki <keihanmuzaki0480@gmail.com>
2025-09-03 21:39:50 +0800

    inheritence

--- javaFiles/Abstraction.java
+++ javaFiles/Abstraction.java
@@ -4,3 +4,23 @@ public abstract class Abstraction {
     abstract void calculateArea();
     abstract void calculateCircumference();
 }
+
+class Square extends Abstraction {
+    private double side;
+
+    public Square(double side) {
+        this.side = side;
+    }
+
+    @Override
+    void calculateArea() {
+        double area = side * side;
+        System.out.println("Area of Square: " + area);
+    }
+
+    @Override
+    void calculateCircumference() {
+        double circumference = 4 * side;
+        System.out.println("Circumference of Square: " + circumference);
+    }
+}
--- javaFiles/Inheritence.java
+++ javaFiles/Inheritence.java
@@ -1,5 +1,19 @@
 package javaFiles;
 
-public class Inheritence  {
-    
+public class Inheritence {
+    String category = "Roller Coasters";
+    public void startRide() {
+        System.out.println("The ride has started!");
+    }
+}
+
+class ThemeParkRide extends Inheritence {
+    private String rideName = "Bumble Bee";
+
+    public static void main(String[] args) {
+        ThemeParkRide ride = new ThemeParkRide();
+
+        ride.startRide();
+        System.out.println("Ride Name: " + ride.rideName);
+    }
 }

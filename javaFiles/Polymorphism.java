76eee95a4e49a020b61d4f474b506b9fe23c14c9
Kmuzaki <keihanmuzaki0480@gmail.com>
2025-09-03 21:50:07 +0800

    addPolyCarJava

--- javaFiles/Polymorphism.java
+++ javaFiles/Polymorphism.java
@@ -1,8 +1,29 @@
 package javaFiles;
 
 public class Polymorphism {
-    String name;
     public void carHorn() {
         System.out.println("Beep Beep!");
+    }
+}
+
+class car extends Polymorphism {
+    public void carHorn() {
+        System.out.println("Beep beep! I'm a car!");
+    }
+}
+
+class truck extends Polymorphism {
+    public void carHorn() {
+        System.out.println("Honk Honk!");
+    }
+}
+
+class Start {
+    public static void main(String[] args) {
+        Polymorphism myVehicle1 = new car();
+        Polymorphism myVehicle2 = new truck();
+
+        myVehicle1.carHorn();
+        myVehicle2.carHorn();
     }
 }
